f418119034950ed2b7a5f471c0466e4e7a5d5968
Khadymba <mbackekhady11@gmail.com>
2023-09-18 22:31:19 -0500

    second commit

--- guess.js
+++ guess.js
@@ -1,19 +1,16 @@
 const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-// Initialize a variable to keep track of the number of guesses
 let numberOfGuesses = 0;
 
-// Prompt the user to guess the number
+
 while (true) {
   const userGuess = prompt("Guess the number between 1 and 10:");
-
-  // Convert the user's input to a number
   const guess = parseInt(userGuess);
 
-  // Check if the input is a valid number
+  
   if (isNaN(guess) || guess < 1 || guess > 10) {
     console.log("Please enter a valid number between 1 and 10.");
-    continue; // Continue the loop if the input is invalid
+    continue; 
   }
 
   numberOfGuesses++; 
@@ -27,4 +24,4 @@ while (true) {
     console.log("Try a lower number.");
   }
 }
-// test
+
